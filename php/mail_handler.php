033e52273b0826c7826d75e645b9e2c7571fd106
Md Shahabub Alam <nabid.aust37@gmail.com>
2023-08-25 15:40:20 +0200

    Update mail_handler.php

--- php/mail_handler.php
+++ php/mail_handler.php
@@ -1,15 +1,12 @@
 <?php
 /* Configuration */
-/*your web-mail*/
-$mailto  = 'msa.nabid.cse@gmail.com';
-
-
-$name     	= strip_tags($_POST['first_name']);
-$sub     	= strip_tags($_POST['sub']);
-$email      = strip_tags($_POST['email']);
-$comments   = strip_tags($_POST['message']);
-
+$mailto = 'nabid.aust37@gmail.com';
 $subject = "[Contact Form]";
+
+$name = strip_tags($_POST['first_name']);
+$sub = strip_tags($_POST['sub']);
+$email = strip_tags($_POST['email']);
+$comments = strip_tags($_POST['message']);
 
 // HTML for email to send submission details
 $body = "
@@ -20,12 +17,6 @@ $body = "
 <b>Email</b>: $email<br>
 ";
 
-
-// Success Message
-$success = "Sent";
-$error = "Failed";
-
-
 $headers = "From: $name <$email> \r\n";
 $headers .= "Reply-To: $email \r\n";
 $headers .= "MIME-Version: 1.0\r\n";
@@ -33,19 +24,14 @@ $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 $headers2 = "From:" . $mailto;
 $message = "<html><body>$body</body></html>";
 
-if(empty($name) || empty($sub) || empty($email) || empty($comments) ){
-
-    
-    header("Location: https://nabidalam.github.io/"); //go to home page
+if (empty($name) || empty($sub) || empty($email) || empty($comments)) {
+    header("Location: https://nabidalam.github.io/"); // go to home page
     die();
-    
-}
-
-else {
-
+} else {
     if (mail($mailto, $subject, $message, $headers)) {
-        header("Location: https://nabidalam.github.io?success=true");
+        header("Location: https://nabidalam.github.io/?success=true#contact-form");
     } else {
-        echo "$error"; // failure
+        echo "Failed"; // failure
     }
 }
+?>
